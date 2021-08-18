a1225b7a019bc4c32255dfa736e4857adbfa42ff
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-08-18 17:38:20 -0500

    wrong branch but w/e

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello Node!");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    profileDataArgs.forEach(profileItem => 
+        console.log(profileItem)
+    );
+};
