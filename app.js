308a72fcaeacb04f3a7a54f5e3b1aba04d281d12
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-08-22 19:22:48 -0500

    added a question

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const fs = require('fs'); 
+const inquirer = require('inquirer');
+// const fs = require('fs'); 
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-const generatePage = require('./src/page-template');
+//   console.log('Portfolio complete! Check out index.html to see the outcome.')
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the outcome.')
-});
+inquirer 
+  .prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
