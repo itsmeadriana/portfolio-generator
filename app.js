e9ca099f3026d5f06e2ff4c4beb85d0334ac7183
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-08-22 23:43:58 -0500

    expanded html template capabilities

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const inquirer = require('inquirer');
-// const fs = require('fs'); 
 
-// const generatePage = require('./src/page-template');
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the outcome.')
-// });
+const fs = require('fs'); 
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt ([
@@ -132,5 +126,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData)
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
